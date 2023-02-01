3895e8505bf151ce6e25e19254f61f46194680fb
JCSH <joshmatsbux@gmail.com>
2023-02-01 11:31:37 -1000

    added seeds content

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO movies (movie_name)
+VALUES
+
+("24 x 36: A Movie About Movie Posters (2016) "),
+("The 10th Victim (1965)"),
+("100 Feet (2008)"),
+("12 Years a Slave (2013) "),
+("13 Conversations About One Thing (2002)"),
+("1408 (2007)"),
+("1776 (1972)"),
+("1917 (2019)"),
+("1990: The Bronx Warriors (1982)"),
+("The 2000 Year Old Man (1975)"),
+("20,000 Leagues Under the Sea (1954)"),
+("2001: A Space Odyssey (1968)"),
+("2010: The Year We Make Contact (1984)"),
+("2019: After the Fall of New York (1983)"),
+("2069: A Sex Odyssey (1974)"),
+("20th Century Fox: The 1st 50 Years (1997)"),
+("20th Century Fox: The Blockbuster Years (2000)"),
+("25th Hour (2002)"),
+("3 Dead Girls (2007)"),
+("30 Days of Night (2007)"),
+("31 (2016) "),
+("The 39 Steps (1935)"),
+("4 Dollars of Revenge (1968)");
+
